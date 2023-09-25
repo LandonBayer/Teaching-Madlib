44b1397c8bdf861539bd0b5d71d5d38851f55ed6
LandonBayer <landonbayer0@gmail.com>
2023-09-25 15:36:15 -0400

    landons section

--- B.py
+++ B.py
@@ -2,12 +2,14 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-number_between_1_and_9000 = 0 # assign some number or string at the end of each of these lines
-adjective_1 = ""
-noun_1 = ""
-famous_person = ""
-adjective_2 = ""
-very_large_number = 0
-noun_2 = ""
-adjective_2 = ""
-large_store = ""
+number_between_1_and_9000 = (
+    8708  # assign some number or string at the end of each of these lines
+)
+adjective_1 = "overly"
+noun_1 = "imposter"
+famous_person = "Luke Maxwell"
+adjective_2 = "komplex"
+very_large_number = 485898
+noun_2 = "Anthony"
+adjective_2 = "thick"
+large_store = "IKEA"
