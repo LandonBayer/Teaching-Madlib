e8fa9287063b1041da96b603da87f0bea1da8043
AZY24 <118216231+24-azy@users.noreply.github.com>
2023-09-25 15:44:33 -0400

    testing

--- A.py
+++ A.py
@@ -3,11 +3,11 @@
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
 year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+wacky_robot_name = "robn't"
+adjective_1 = "dilapidated"
+violent_verb = "defenestrate"
+tech_company = "apple"
+dollar_amount = "42"
+adjective_2 = "middle eastern"
+noun_1 = "magma"
+large_store = "ikea"
